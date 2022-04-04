58cfdc10e111faa50cd2a916e5a02f7fa8d042c6
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-04-04 23:04:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['083811034750','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['083168004413'] //another owner number
+global.premium = ['083811034750'] //premium number
+global.pengguna = 'Wangsaf' //username
+global.botnma = 'WANGSAF BOT-MD' //bot name
+global.ownernma = 'WANGSAF' //owner name
+global.packname = 'WANGSAF Bot' //sticker package name
+global.author = 'WA-BOT-MD' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
